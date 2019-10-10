6c24b8f8025d708e32d279e46306030ccbe5d0ca
Ahmed Belgana <ahmed@hashicorp.com>
2019-10-10 17:47:17 -0400

    Delete VM

--- infrastructure-as-code/aws-ec2-instance/main.tf
+++ infrastructure-as-code/aws-ec2-instance/main.tf
@@ -6,12 +6,12 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}a"
-
-  tags = {
-    Name = "${var.name}"
-  }
-}
+// resource "aws_instance" "ubuntu" {
+//   ami           = "${var.ami_id}"
+//   instance_type = "${var.instance_type}"
+//   availability_zone = "${var.aws_region}a"
+// 
+//   tags = {
+//     Name = "${var.name}"
+//   }
+// }
